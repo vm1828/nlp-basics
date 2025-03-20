65d3dcb699316c1cdeb81cf5206bcaf9fb735206
vm1828 <vm18281828@gmail.com>
2025-03-20 21:05:19 +0200

    Update dependencies

--- init.sh
+++ init.sh
@@ -2,3 +2,6 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install wheel
 pip install -r requirements.txt
+python3 -m spacy download en_core_web_sm
+
+python3 -m nltk.downloader -d ./.venv/nltk_data punkt_tab
