0bf6eaff147af74f4b63268179eed87abd1c0607
vm1828 <vm18281828@gmail.com>
2025-04-07 09:38:40 +0300

    Refactor

--- shared/constants.py
+++ shared/constants.py
@@ -1,3 +1,3 @@
 TXT_FILE = 'data/pride_and_prejudice.txt'
 PUNCTUATION = '.;,-“’”:?—‘!()_'
-LINE_TO_EXCLUDE = r'(?i)^\s*CHAPTER\s*[IVXLCDM]+\s*$'
+CHAPTER_LINE = r'(?i)^\s*CHAPTER\s*[IVXLCDM]+[\s.]*$'
