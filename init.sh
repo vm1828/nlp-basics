f67467503d4a5b1138badf9b50cc9080a5e021ac
vm1828 <vm18281828@gmail.com>
2025-04-09 19:14:50 +0300

    Add Spacy_basics.ipynb

--- init.sh
+++ init.sh
@@ -7,8 +7,9 @@ if [ ! -d ".venv" ]; then
     source .venv/bin/activate
     pip install wheel
     pip install -r requirements.txt
-    python3 -m spacy download en_core_web_sm
 
+    python3 -m spacy download en_core_web_sm
+    python3 -m spacy download en_core_web_lg
     python3 -m nltk.downloader -d ./.venv/nltk_data punkt_tab
 else
     echo "Project already initialized."
