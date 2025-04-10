6529934f6bdfa0d0433978f192fff6efc040b117
vm1828 <vm18281828@gmail.com>
2025-04-10 12:14:42 +0300

    Add hp characters

--- shared/constants.py
+++ shared/constants.py
@@ -3,8 +3,9 @@ import sys
 from unicodedata import category
 
 
-TXT_PNP = os.path.join('data', 'pnp.txt')
+TXT_PNP = os.path.join('data', 'pnp', 'pnp.txt')
 JSON_CHARACTERS_PNP = os.path.join('data', 'pnp_characters.json')
+JSON_CHARACTERS_HP = os.path.join('data', 'hp_characters.json')
 DIR_PNP = os.path.join('data', 'pnp')
 DIR_HP = os.path.join('data', 'hp')
 
