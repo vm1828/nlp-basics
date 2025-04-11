484db78b5bee220146734566a1f59eabd70d5ff2
vm1828 <vm18281828@gmail.com>
2025-04-11 12:32:20 +0300

    Use EntityRuler to create rule-based NER model

--- shared/constants.py
+++ shared/constants.py
@@ -2,15 +2,19 @@ import os
 import sys
 from unicodedata import category
 
-
+# paths
 TXT_PNP = os.path.join('data', 'pnp', 'pnp.txt')
 JSON_CHARACTERS_PNP = os.path.join('data', 'pnp_characters.json')
 JSON_CHARACTERS_HP = os.path.join('data', 'hp_characters.json')
+JSON_CHARACTERS_HP_GENERATED = os.path.join(
+    'data', 'hp_characters_generated.json')
 DIR_PNP = os.path.join('data', 'pnp')
 DIR_HP = os.path.join('data', 'hp')
+DIR_MODELS_SPACY = os.path.join('models', 'spacy')
 
 # utf-8 punctuation
 PUNCTUATION = ''.join([chr(i) for i in range(sys.maxunicode)
                        if category(chr(i)).startswith("P") and chr(i) != '-'])
 
+# regex patterns
 PATTERN_CHAPTER_LINE = r"(?i)\s*CHAPTER\s*\d{1,2}\s*"
--- shared/utils.py
+++ shared/utils.py
@@ -1,3 +1,4 @@
+import json
 import re
 
 from shared.constants import PUNCTUATION
@@ -8,3 +9,9 @@ def remove_punctuation(text: str) -> str:
         str.maketrans("", "", PUNCTUATION))
     text = re.sub(r'-{2,}', ' ', text)  # remove hyphens if more than 1
     return text
+
+
+def load_json(file: str) -> dict | list:
+    with open(file, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    return data
